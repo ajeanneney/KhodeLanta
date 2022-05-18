90ba580df03527da6197cca0f428aa7e829f9096
ajeanneney <antoine.jeanneney@gmail.com>
2022-05-18 09:46:41 +0200

    Revert "menage"
    This reverts commit 200f37ca553744ab40c03d6f2aa69775ea43a7ac.

--- src/main/java/fr/isep/homeexchangemanager/controller/ConnexionController.java
+++ src/main/java/fr/isep/homeexchangemanager/controller/ConnexionController.java
@@ -26,10 +26,7 @@ public class ConnexionController {
     public String connexion(
             @RequestParam(name = "mail", defaultValue = "") String mail,
             @RequestParam(name = "password", defaultValue = "") String password,
-            @CookieValue(value = "userId", defaultValue = "") String userId,
             HttpServletResponse response){
-
-        if(Objects.equals(userId, "") || userDao.findById(Long.valueOf(userId)).isEmpty()){return "redirect:connexion";}
 
         if(!Objects.equals(mail, "") && !Objects.equals(password, "")) {
             User user = userDao.findByMail(mail);
@@ -43,14 +40,16 @@ public class ConnexionController {
         }
         return "connexion";
     }
-    
+
+    @RequestMapping(value = "/connected")
+    public String connected(){
+        return "connected";
+    }
+
     @RequestMapping(value = "/signup")
     public String signUp(
             ServletRequest request,
-            HttpServletResponse response,
-            @CookieValue(value = "userId", defaultValue = "") String userId){
-
-        if(Objects.equals(userId, "") || userDao.findById(Long.valueOf(userId)).isEmpty()){return "redirect:connexion";}
+            HttpServletResponse response){
 
         Map<String, String[]> paramMap = request.getParameterMap();
 
--- src/main/java/fr/isep/homeexchangemanager/controller/ReservationController.java
+++
@@ -1,4 +0,0 @@
-package fr.isep.homeexchangemanager.controller;
-
-public class ReservationController {
-}
