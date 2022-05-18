8054c9c9c375ebc7829af25aa4a20f1fc83d72b4
ajeanneney <antoine.jeanneney@gmail.com>
2022-05-18 09:46:10 +0200

    Revert "ajouter la recherche des maisons"
    This reverts commit 88ec042072c3baea5d6e0ffdf1a664164ab4c093.

--- src/main/java/fr/isep/homeexchangemanager/controller/HomeController.java
+++ src/main/java/fr/isep/homeexchangemanager/controller/HomeController.java
@@ -5,6 +5,7 @@ import fr.isep.homeexchangemanager.dao.UserRepository;
 import fr.isep.homeexchangemanager.entities.House;
 import fr.isep.homeexchangemanager.entities.User;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.jpa.repository.support.SimpleJpaRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.CookieValue;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,24 +13,27 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import javax.servlet.ServletRequest;
 import java.util.Map;
 import java.util.Objects;
+import java.util.Optional;
 
 @Controller
-public class HouseController {
+public class HomeController {
 
     @Autowired
     private UserRepository userDao;
     @Autowired
     private HouseRepository houseDao;
 
-    @RequestMapping(value = "/newhouse")
-    public String newHouse(
+    @RequestMapping(value = "/newhome")
+    public String newHome(
             @CookieValue(value = "userId", defaultValue = "") String userId,
             ServletRequest request){
 
         if(Objects.equals(userId, "") || userDao.findById(Long.valueOf(userId)).isEmpty()){return "redirect:connexion";} //si pas connecté retour page connexion
 
+        System.out.println("coucouc2");
         Map<String, String[]> paramMap = request.getParameterMap();
         if(paramMap.containsKey("title") && paramMap.containsKey("description")){
+            System.out.println("coucou");
             User user = userDao.findById(Long.parseLong(userId)).orElse(null);
             House house = new House(
                     user,
@@ -40,6 +44,6 @@ public class HouseController {
         }
 
 
-        return "newhouse";
+        return "newhome";
     }
 }
--- src/main/webapp/WEB-INF/jsp/newhome.jsp
+++ src/main/webapp/WEB-INF/jsp/newhome.jsp
@@ -9,7 +9,7 @@
 </head>
 <h1>Ajouter une nouvelle maison</h1>
 
-<form method="post" action="/newhouse">
+<form method="post" action="/newhome">
     <div>
         <input type="Nom de la maison" class="input2" name="title" id="title" placeholder="Nom de la maison">
     </div>
